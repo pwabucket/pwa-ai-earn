bab535fc8bd8ab6c4bc5ceb3a4bb7a86cdec6e04
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-05 23:31:09 +0100

    feat: Optimized rendering of timeline

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,15 +5,24 @@ import { cn } from "../lib/utils";
 export default function Modal({
   onOpenChange,
   children,
+  overlayRef,
+  contentRef,
 }: {
   onOpenChange: (open: boolean) => void;
   children: React.ReactNode;
+  overlayRef: React.RefObject<HTMLDivElement>;
+  contentRef: React.RefObject<HTMLDivElement>;
 }) {
   return (
     <Dialog.Root open={true} onOpenChange={onOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/60 z-40 overflow-y-auto grid place-items-center py-10">
+        <Dialog.Overlay
+          ref={overlayRef}
+          className="fixed inset-0 bg-black/60 z-40 overflow-y-auto grid place-items-center py-10"
+        >
           <Dialog.Content
+            ref={contentRef}
+            onInteractOutside={(ev) => ev.preventDefault()}
             className={cn(
               "bg-neutral-900 text-white rounded-2xl p-6",
               "w-full max-w-md shadow-2xl border border-neutral-800"
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,3 +1,5 @@
+import { Virtuoso } from "react-virtuoso";
+
 import Currency from "./Currency";
 import Radius from "./Radius";
 import { formatDate } from "../utils/dateUtils";
@@ -16,39 +18,51 @@ interface TimelineDay {
 export default function Timeline({ timeline }: { timeline: TimelineDay[] }) {
   return (
     <div className="flex flex-col gap-2">
-      {timeline.map((day, index) => (
-        <div key={index} className="flex gap-2 p-4 bg-neutral-800 rounded-xl">
-          <Radius
-            max={timeline.length}
-            position={day.index}
-            className={day.compound ? "stroke-pink-500" : "stroke-green-500"}
-          />
-          <div className="flex flex-col gap-2 grow min-w-0">
-            <h4 className="text-sm text-pink-500 font-bold">
-              {formatDate(day.date)}
-            </h4>
-            <div className="flex flex-col gap-1">
-              <span className="text-sm text-purple-400">
-                TP: <Currency value={day.activeInvestments} />
-              </span>
-              <span className="text-sm text-blue-400">
-                Invested: <Currency value={day.totalInvested} />
-              </span>
-              <span className="text-sm text-yellow-400">
-                Reinvested: <Currency value={day.balanceReinvested} />
-              </span>
+      <Virtuoso
+        style={{ height: "400px" }}
+        data={timeline}
+        itemContent={(index, day) => (
+          <div className="pb-2">
+            <div
+              key={index}
+              className="flex gap-4 p-4 bg-neutral-800 rounded-xl"
+            >
+              <Radius
+                max={timeline.length}
+                position={day.index}
+                className={
+                  day.compound ? "stroke-pink-500" : "stroke-green-500"
+                }
+              />
+              <div className="flex flex-col gap-1 grow min-w-0">
+                <h4 className="text-sm text-pink-500 font-bold">
+                  {formatDate(day.date)}
+                </h4>
+                <div className="flex flex-col gap-1">
+                  <span className="text-sm text-purple-400">
+                    TP: <Currency value={day.activeInvestments} />
+                  </span>
+                  <span className="text-sm text-blue-400">
+                    Invested: <Currency value={day.totalInvested} />
+                  </span>
+                  <span className="text-sm text-yellow-400">
+                    Reinvested: <Currency value={day.balanceReinvested} />
+                  </span>
+                </div>
+              </div>
 
-              <span className="text-sm text-green-400">
-                Daily Profit:{" "}
-                <Currency value={day.currentDailyProfit} prefix="+" />
-              </span>
-              <span className="text-sm text-green-400">
-                Balance: <Currency value={day.availableBalance} />
-              </span>
+              <div className="flex flex-col gap-2 shrink-0 text-right">
+                <span className="text-sm text-green-400">
+                  <Currency value={day.currentDailyProfit} prefix="+" />
+                </span>
+                <span className="text-sm text-green-400">
+                  <Currency value={day.availableBalance} />
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        )}
+      />
     </div>
   );
 }
