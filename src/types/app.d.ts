1c6e6d519b8b882f8e36cf631f2377cfa4845bd3
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-21 09:59:57 +0100

    feat: Removed unused types

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -13,20 +13,6 @@ export interface Account {
   transactions: Transaction[];
 }
 
-export interface Investment {
-  id: string;
-  amount: number;
-  date: Date;
-  isSimulated?: boolean;
-}
-
-export interface Withdrawal {
-  id: string;
-  amount: number;
-  date: Date;
-  isSimulated?: boolean;
-}
-
 export interface GoogleApiToken extends google.accounts.oauth2.TokenResponse {
   expires_at: number;
   refresh_token?: string;
